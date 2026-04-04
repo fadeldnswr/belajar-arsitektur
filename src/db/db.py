658b8552606b8c739a0fb32ae7d979b38bcfb4b1
fadeldnswr <fadel.achmad@ui.ac.id>
2026-04-04 09:27:41 +0700

    Create SQLite database connection

---
+++ src/db/db.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from src.db.config import settings
+
+# Load database settings
+DATABASE_URL = settings.DATABASE_URL
+
+# Create SQLAlchemy engine
+engine = create_engine(
+  DATABASE_URL, connect_args={
+    "check_same_thread": False # Required for SQLite to allow multiple threads to access the database
+  }
+)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(
+  autocommit=False,
+  autoflush=False,
+  bind=engine
+)
+
+# Create a base class for declarative class definitions
+Base = declarative_base()
+
+# Dependency to get a database session
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
