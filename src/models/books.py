7601c0e630ce9db6060550273294613ac4845986
fadeldnswr <fadel.achmad@ui.ac.id>
2026-04-04 17:26:43 +0700

    Refactor DB schema

--- src/models/books.py
+++ src/models/books.py
@@ -3,39 +3,12 @@ Book model using Pydantic for data validation and serialization.
 '''
 import uuid 
 
-from pydantic import BaseModel
-from typing import List, Optional
 from src.db.db import Base
 from sqlalchemy import Column, Integer, String, DateTime, func
 from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 
-# Define books metadata
-class Book(BaseModel):
-  id: uuid.UUID
-  title: str
-  author: str
-  published_year: int
-  genre: Optional[str] = None
-  created_at: Optional[datetime] = None
-  
-  class Config:
-    from_attributes = True
-
-# Define books response model
-class BooksResponse(BaseModel):
-  data: List[Book]
-  
-  class Config:
-    from_attributes = True
-
-# Define message for successful deletion of a book entry
-class Message(BaseModel):
-  status: int
-  message: str
-  data: Optional[List] = None
-
-# Define database schema for books
+# Define base database schema for books
 class BookDB(Base):
   __tablename__ = "books" # Table name in the database
   
